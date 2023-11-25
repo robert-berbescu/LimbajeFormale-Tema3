c47e34e4f23bd04e8768f17a286777ab636feaaa
Robert Berbescu <contact@robertberbescu.ro>
2023-11-25 20:38:31 +0200

    add: tabela de salt map

--- LimbajeFormale-Tema3.cpp
+++ LimbajeFormale-Tema3.cpp
@@ -1,8 +1,105 @@
 #include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-	
+    //system("bison -d reguli.y --report=state");
+
+    unordered_map<string, vector<string>> actiuni;
+
+    ifstream f("reguli.output");
+
+    bool stateNou = false;
+    string line;
+    int nrState = 0;
+    int undeSarim;
+    string token;
+    string rSauD;
+
+
+    unordered_map<string, vector<string>> salt;
+
+    while (!f.eof()) { 
+
+        if (stateNou == false) {
+            getline(f, line);
+            stateNou = false;
+        }
+        if (line.find("state") != std::string::npos && line.find("go to") == std::string::npos) {
+            if (line.length() == 7) {
+                nrState = stoi(line.substr(6, 7));
+            }
+            else {
+                nrState = stoi(line.substr(6, 8));
+            }
+            cout << "state nr: " << nrState << endl;
+
+
+            while (getline(f, line) && !(line.find("state") != std::string::npos && line.find("go to") == std::string::npos)) {
+                
+                //Cautam linii care contin "shift" sau "reduce"
+                if (line.find("shift") != std::string::npos || line.find("reduce") != std::string::npos) {
+
+                    if (line.find("shift") != std::string::npos) {
+                        rSauD = "shift";
+					}
+					else {
+						rSauD = "reduce";
+                    }
+
+                    if (line.substr(4, 4) == "$end") //in loc de $end punem $
+                    {
+                        token = "$";
+                    }
+                    else if (line.substr(4,8)=="$default") {
+                        token = "$default";
+                    }
+                    else {
+                        token = line.substr(5, 1);
+                    }
+
+                    if (line.length() == 33) {
+                        undeSarim = stoi(line.substr(32, 33));
+                    }
+                    else {
+                        undeSarim = stoi(line.substr(32, 34));
+                    }
+
+                    //cout<<token<<" "<<rSauD<<" "<< undeSarim << endl;
+                }
+
+                //Cautam pentru tabela de salt (gata)
+                if (line.find("go to state") != std::string::npos && line.find("'") == std::string::npos && line.find("$") == std::string::npos) {
+                    
+                    token = line.substr(4, 1);
+
+                    if (line.length() == 20) {
+                        undeSarim = stoi(line.substr(19, 1));
+                    }
+                    else {
+                        undeSarim = stoi(line.substr(19, 2));
+                    }
+
+                    if (salt.find(token) == salt.end()) {
+						salt[token] = vector<string>();
+                    }
+
+                    salt[token].push_back(to_string(undeSarim));
+
+				}
+
+            }
+            stateNou = true;
+        }
+        cout << endl;
+    }
+
+
+
+        return 0;
 }
---
+++ reguli.y
@@ -0,0 +1,36 @@
+%{
+#include <stdio.h>
+#include <stdlib.h>
+%}
+
+%%
+
+E:    E '+' T
+    | T
+;
+
+T:    T '*' F
+    | F
+;
+
+F:    '(' E ')'
+    | 'a'
+    | '-' '(' E ')'
+;
+
+
+%%
+
+int yyerror(char *s) {
+    fprintf(stderr, "%s\n", s);
+    return 1;
+}
+
+int main() {
+    if (yyparse())
+        fprintf(stderr, "Successful parsing.\n");
+    else
+        fprintf(stderr, "Error found.\n");
+
+    return 0;
+}
