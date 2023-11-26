96c7b6e08c4086583ddf150c5b3bd76755b9d782
Robert Berbescu <contact@robertberbescu.ro>
2023-11-26 16:10:51 +0200

    add: tabela de actiuni si tabela de salt

--- LimbajeFormale-Tema3.cpp
+++ LimbajeFormale-Tema3.cpp
@@ -10,25 +10,53 @@ int main()
 {
     //system("bison -d reguli.y --report=state");
 
-    unordered_map<string, vector<string>> actiuni;
+    
 
     ifstream f("reguli.output");
 
     bool stateNou = false;
     string line;
     int nrState = 0;
-    int undeSarim;
+    string undeSarim;
     string token;
     string rSauD;
 
-
+    unordered_map<string, vector<string>> actiuni;
     unordered_map<string, vector<string>> salt;
+    vector<string> terminals;
+    vector<string> neterminale;
+
+    terminals.push_back("(");
+    terminals.push_back(")");
+    terminals.push_back("*");
+    terminals.push_back("+");
+    terminals.push_back("-");
+    terminals.push_back("a");
+    terminals.push_back("$");
+
+    neterminale.push_back("E");
+    neterminale.push_back("T");
+    neterminale.push_back("F");
+
+    actiuni["("] = vector<string>();
+    actiuni[")"] = vector<string>();
+    actiuni["*"] = vector<string>();
+    actiuni["+"] = vector<string>();
+    actiuni["-"] = vector<string>();
+    actiuni["a"] = vector<string>();
+    actiuni["$"] = vector<string>();
+
+    salt["E"] = vector<string>();
+    salt["T"] = vector<string>();
+    salt["F"] = vector<string>();
+    
 
     while (!f.eof()) { 
 
         if (stateNou == false) {
             getline(f, line);
             stateNou = false;
+
         }
         if (line.find("state") != std::string::npos && line.find("go to") == std::string::npos) {
             if (line.length() == 7) {
@@ -40,16 +68,37 @@ int main()
             cout << "state nr: " << nrState << endl;
 
 
+            for (auto& entry : actiuni) {
+                entry.second.push_back("#");
+            }
+
+            for (auto& entry : salt) {
+                entry.second.push_back("#");
+            }
+            
+
             while (getline(f, line) && !(line.find("state") != std::string::npos && line.find("go to") == std::string::npos)) {
                 
-                //Cautam linii care contin "shift" sau "reduce"
-                if (line.find("shift") != std::string::npos || line.find("reduce") != std::string::npos) {
+                undeSarim = "#";
+
+                //Cautam "Terminals"
+                if (line.find("Terminals") != std::string::npos) {
+                    //de facut
+                }
+
+
+                //Cautam linii care contin "shift" sau "reduce" sau "accept"
+                if (line.find("shift") != std::string::npos || line.find("reduce") != std::string::npos || line.find("$default  accept") != std::string::npos) {
+
 
                     if (line.find("shift") != std::string::npos) {
-                        rSauD = "shift";
+                        rSauD = "d";
 					}
-					else {
-						rSauD = "reduce";
+					else if(line.find("reduce") != std::string::npos) {
+						rSauD = "r";
+                    }
+                    else {
+                        rSauD = "acc";
                     }
 
                     if (line.substr(4, 4) == "$end") //in loc de $end punem $
@@ -63,14 +112,46 @@ int main()
                         token = line.substr(5, 1);
                     }
 
-                    if (line.length() == 33) {
-                        undeSarim = stoi(line.substr(32, 33));
-                    }
-                    else {
-                        undeSarim = stoi(line.substr(32, 34));
+                    if (rSauD == "d") {
+                        if (line.length() == 33) {
+                            undeSarim = line.substr(32, 1);
+                        }
+                        else {
+                            undeSarim = line.substr(32, 2);
+                        }
+					}
+					else if(rSauD=="r") //daca avem reduce
+                    {
+                        if (line.length() == 37) {
+							undeSarim = line.substr(32, 1);
+						}
+                        else {
+							undeSarim = line.substr(32, 2);
+						}
                     }
 
-                    //cout<<token<<" "<<rSauD<<" "<< undeSarim << endl;
+
+                    //cout << "Linia " << line << " are " << line.length() << endl;
+
+                    if (line.length() == 20) {
+                        undeSarim = "";
+                    }
+
+                    
+                    //cout << token << " " << rSauD << " " << undeSarim << endl;
+                   
+                    if (token == "$default") {
+                        for (auto& entry : actiuni) {
+                            entry.second.at(nrState) = (rSauD + undeSarim);
+                        }
+                    }
+                    else if (undeSarim == "accept") {
+                        
+                        actiuni[token].at(nrState) = "acc";
+                    }
+                    else {
+                        actiuni[token].at(nrState) = (rSauD + undeSarim);
+                    }                      
                 }
 
                 //Cautam pentru tabela de salt (gata)
@@ -79,22 +160,42 @@ int main()
                     token = line.substr(4, 1);
 
                     if (line.length() == 20) {
-                        undeSarim = stoi(line.substr(19, 1));
+                        undeSarim = line.substr(19, 1);
                     }
                     else {
-                        undeSarim = stoi(line.substr(19, 2));
+                        undeSarim = line.substr(19, 2);
                     }
 
-                    if (salt.find(token) == salt.end()) {
-						salt[token] = vector<string>();
-                    }
-
-                    salt[token].push_back(to_string(undeSarim));
+                    salt[token].at(nrState) = (undeSarim);
 
 				}
 
             }
+            cout << endl;
             stateNou = true;
+        }
+    }
+
+
+    //Afisare tabela de actiuni
+    cout << "Tabela de actiuni: " << endl;
+   
+    for (auto& entry : actiuni) {
+        cout<<entry.first<<" -> ";
+        for (int i = 0;i < entry.second.size();i++)
+        {
+			cout << entry.second.at(i) << " ";
+		}
+        cout << endl;
+    }
+
+    //Afisare tabela de salt
+    cout << endl << "Tabela de salt: " << endl;
+    for (auto& entry : salt) {
+        cout<<entry.first<<" -> ";
+        for (int i = 0;i < entry.second.size();i++)
+        {
+            cout<<entry.second.at(i)<<" ";
         }
         cout << endl;
     }
