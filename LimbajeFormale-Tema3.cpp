e07f5e31efe9cf451fa7cd07b1ebec901abd81e4
Robert Berbescu <contact@robertberbescu.ro>
2023-11-26 18:31:35 +0200

    add: afisare tabele in consola

--- LimbajeFormale-Tema3.cpp
+++ LimbajeFormale-Tema3.cpp
@@ -182,8 +182,6 @@ int main()
             else {
                 nrState = stoi(line.substr(6, 8));
             }
-            //cout << "state nr: " << nrState << endl;
-
 
             for (auto& entry : actiuni) {
                 entry.second.push_back("#");
@@ -193,14 +191,11 @@ int main()
                 entry.second.push_back("#");
             }
             
-
             while (getline(f, line) && !(line.find("state") != std::string::npos && line.find("go to") == std::string::npos)) {
                 
                 undeSarim = "#";
-
                 //Cautam linii care contin "shift" sau "reduce" sau "accept"
                 if (line.find("shift") != std::string::npos || line.find("reduce") != std::string::npos || line.find("$default  accept") != std::string::npos) {
-
 
                     if (line.find("shift") != std::string::npos) {
                         rSauD = "d";
@@ -241,15 +236,9 @@ int main()
 						}
                     }
 
-
-                    //cout << "Linia " << line << " are " << line.length() << endl;
-
                     if (line.length() == 20) {
                         undeSarim = "";
                     }
-
-                    
-                    //cout << token << " " << rSauD << " " << undeSarim << endl;
                    
                     if (token == "$default") {
                         for (auto& entry : actiuni) {
@@ -280,33 +269,42 @@ int main()
                     salt[token].at(nrState) = (undeSarim);
 
 				}
-
             }
-            //cout << endl;
             stateNou = true;
         }
     }
 
-
     //Afisare tabela de actiuni
-    cout << "Tabela de actiuni: " << endl;
-   
+    cout << "Tabela de actiuni si de salt generate de Bison: " << endl<<endl;
+  
+    cout << "      ";
+    for (int i = 0;i < actiuni.begin()->second.size();i++)
+    {
+        int lungime = to_string(i).size();
+        cout <<i << setw(5 - lungime) << " ";
+	}  
+    cout << endl<<endl;
+
     for (auto& entry : actiuni) {
-        cout<<entry.first<<" -> ";
+        cout<<entry.first<<"     ";
+        int lungime=0;
         for (int i = 0;i < entry.second.size();i++)
         {
-			cout << entry.second.at(i) << " ";
+            lungime = entry.second.at(i).size();
+			cout << entry.second.at(i)<<setw(5-lungime)<< " ";
 		}
         cout << endl;
     }
 
     //Afisare tabela de salt
-    cout << endl << "Tabela de salt: " << endl;
+    cout << endl<<endl;
     for (auto& entry : salt) {
-        cout<<entry.first<<" -> ";
+        cout<<entry.first<<"     ";
+        int lungime = 0; 
         for (int i = 0;i < entry.second.size();i++)
         {
-            cout<<entry.second.at(i)<<" ";
+            lungime = entry.second.at(i).size();
+            cout<<entry.second.at(i)<<setw(5-lungime)<<" ";
         }
         cout << endl;
     }
@@ -315,8 +313,7 @@ int main()
 
     // APD - TEMA 2
     cout << endl << endl;
-    cout << "APD - TEMA 2" << endl;
-
+    cout << "APD - TEMA 2" << endl<<endl;
 
     stiva.push("$");
     stiva.push("0");
